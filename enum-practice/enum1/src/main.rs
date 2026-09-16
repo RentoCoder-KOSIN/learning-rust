16a6b257606b8b79b3ccf095ee25cf880918e48a
= <=>
2026-09-16 15:30:00 +0900

    refactor: enum1

--- enum-practice/enum1/src/main.rs
+++ enum-practice/enum1/src/main.rs
@@ -5,36 +5,38 @@ enum Message {
     ChangeColor(i32, i32, i32),
 }
 
-fn show_message(message: &Message) {
-    match message {
-        Message::Quit => {
-            println!("Quit");
-        }
+impl Message {
+    fn show_message(message: &Message) {
+        match message {
+            Message::Quit => {
+                println!("Quit");
+            }
 
-        Message::Move { x, y } => {
-            println!("x = {x}, y = {y}");
-        }
+            Message::Move { x, y } => {
+                println!("x = {x}, y = {y}");
+            }
 
-        Message::Write(text) => {
-            println!("{text}");
-        }
+            Message::Write(text) => {
+                println!("{text}");
+            }
 
-        Message::ChangeColor(r, g, b) => {
-            println!("{r}, {g}, {b}");
+            Message::ChangeColor(r, g, b) => {
+                println!("{r}, {g}, {b}");
+            }
         }
     }
 }
 
 fn main() {
     let mut message = Message::Quit;
-    show_message(&message);
+    Message::show_message(&message);
 
     message = Message::Move { x: 30, y: 60 };
-    show_message(&message);
+    Message::show_message(&message);
 
     message = Message::Write(String::from("hello"));
-    show_message(&message);
+    Message::show_message(&message);
 
     message = Message::ChangeColor(255, 0, 0);
-    show_message(&message);
+    Message::show_message(&message);
 }
