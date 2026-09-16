b337b079a869f1afaf0c76e1e9b744076ee1992f
= <=>
2026-09-16 09:08:19 +0900

    enum-practice

--- enum-practice/src/main.rs
+++ enum-practice/src/main.rs
@@ -1,14 +1,22 @@
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
+enum PortStatus {
+    Open(u16),
+    Closed,
+    Timeout(u64),
+}
+
+fn describe(status: PortStatus) -> String {
+    match status {
+        PortStatus::Open(port) => format!("Port{port} is opened"),
+        PortStatus::Closed => String::from("closed"),
+        PortStatus::Timeout(secs) => format!("Timeout {secs}s"),
     }
 }
 
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-
-    println!("{five:?} ,{six:?}, {none:?}");
+    let status1 = PortStatus::Open(80);
+    let status2 = PortStatus::Closed;
+    let status3 = PortStatus::Timeout(443);
+    println!("{}", describe(status1));
+    println!("{}", describe(status2));
+    println!("{}", describe(status3));
 }
